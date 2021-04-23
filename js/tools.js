4472e55b8c025d912fc1af242bc066e29470f0e6
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-04-24 00:04:06 +1200

    Cleanup and structuring.

--- js/tools.js
+++ js/tools.js
@@ -20,7 +20,7 @@ function meta_check(min, max) {
     let start = Date.now(),
     no_meta = [],
     checked = 0,
-    id_array = story.map(e => e.id);
+    id_array = story_map;
 
     if (min !== undefined) id_array = id_array.filter(e => e >= min);
     if (max !== undefined) id_array = id_array.filter(e => e <= max);
@@ -28,16 +28,40 @@ function meta_check(min, max) {
     let len = id_array.length;
 
     for (let i = 0; i < len; i++, checked++) {
-        if (story[story.map(e => e.id).indexOf(id_array[i])]?.meta === undefined) no_meta.push(i);
+        if (story[story_map.indexOf(id_array[i])]?.meta === undefined) no_meta.push(i);
     }
     console.log(`${no_meta.length}/${checked} scenes are missing metadata (${Date.now() - start}ms)`, no_meta);
 }
 
 function general_debugger() {
     // hopeful
+    let errors = 0;
     if (player.stats.fatigue !== undefined) {
+        errors += 1;
         console.warn("Something assigned 'fatigue' to player.stats!");
         delete player.stats.fatigue;
     }
+    let inn_map = player.inns.map(e => e.id);
+    if (new Set(inn_map).size != inn_map.length) {
+        errors += 1;
+        console.warn("Found duplicate inns in player data, emptying.");
+        player.inns = [];
+    }
+    if (errors == 0) {
+        console.log("General debugger found nothing.");
+    }
     // to be continued...
+}
+
+function ignominy_export(mode, explicit) {
+    if (mode == "story") {
+        let file = new Blob([(JSON.stringify(story, null, 2))], {type: "application/json"}),
+        elem = window.document.createElement("a");
+        elem.href = window.URL.createObjectURL(file);
+        elem.download = `ignominy${version}_${mode}.json`;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+        return;
+    }
 }
