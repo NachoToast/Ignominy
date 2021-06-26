6eefdea815aed24dbeda90d468c5f41ee334d043
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-06-26 17:24:29 +1200

    Refactoring

--- js/start.js
+++ js/start.js
@@ -1,15 +1,15 @@
 function generate_map_menu() {
-    let lore = document.getElementById("initial_lore");
-    let d1 = document.createElement("div"),
-    d2 = document.createElement("div"),
-    d3 = document.createElement("div");
-    d1.id = "location_meta";
-    d1.id = "location_select";
-    d1.id = "location_chose";
-    lore.appendChild(d1);
-    lore.appendChild(d2);
-    lore.appendChild(d3);
-    let content1 = `
+  let lore = document.getElementById('initial_lore');
+  let d1 = document.createElement('div'),
+    d2 = document.createElement('div'),
+    d3 = document.createElement('div');
+  d1.id = 'location_meta';
+  d1.id = 'location_select';
+  d1.id = 'location_chose';
+  lore.appendChild(d1);
+  lore.appendChild(d2);
+  lore.appendChild(d3);
+  let content1 = `
     <div id="location_meta">
         <h1> World Map</h1>
         <div>
@@ -135,84 +135,83 @@ function generate_map_menu() {
         </div>
     </div>
     `;
-    d1.innerHTML = content1;
-    d2.innerHTML = content2;
-    d3.innerHTML = content3;
-    map_sliders = document.getElementsByClassName("map_options");
-    map_outputs = document.getElementsByClassName("map_outputs");
-    maps = document.getElementsByClassName("map_s");
-    zoom_output = document.getElementsByClassName("map_zoom_output");
+  d1.innerHTML = content1;
+  d2.innerHTML = content2;
+  d3.innerHTML = content3;
+  map_sliders = document.getElementsByClassName('map_options');
+  map_outputs = document.getElementsByClassName('map_outputs');
+  maps = document.getElementsByClassName('map_s');
+  zoom_output = document.getElementsByClassName('map_zoom_output');
 }
 function slider_modify_map(layer) {
-    //console.log(layer);
-    if (current_header !== 2) {
-        map_outputs[5 + layer].value = map_sliders[5 + layer].value;
-        maps[5 + layer + 2].style.opacity = map_sliders[5 + layer].value / 100;
-        return;
-    }
-    map_outputs[layer].value = map_sliders[layer].value;
-    maps[layer + 1].style.opacity = map_sliders[layer].value / 100;
+  //console.log(layer);
+  if (current_header !== 2) {
+    map_outputs[5 + layer].value = map_sliders[5 + layer].value;
+    maps[5 + layer + 2].style.opacity = map_sliders[5 + layer].value / 100;
+    return;
+  }
+  map_outputs[layer].value = map_sliders[layer].value;
+  maps[layer + 1].style.opacity = map_sliders[layer].value / 100;
 }
 function direct_modify_map(layer) {
-    map_sliders[layer].value = map_outputs[layer].value;
-    maps[layer+1].style.opacity = map_outputs[layer].value / 100;
+  map_sliders[layer].value = map_outputs[layer].value;
+  maps[layer + 1].style.opacity = map_outputs[layer].value / 100;
 }
 function zoom(type) {
-    if ((zoom_level * 100).toFixed(0) >= 500 && type == 0.1 || (zoom_level * 100).toFixed(0) <= 10 && type == -0.1) return;
-    zoom_level += type;
-    let len = maps.length, mult = 0;
-    if (current_header !== 2) {
-        len = maps.length / 2;
-        mult = 6;
-    }
-    for (let i = 0; i < len; i++) {
-        maps[i + mult].width = base_width * zoom_level;
-        maps[i + mult].height = base_height * zoom_level;
-    }
-    zoom_output[mult / 6].innerText = (zoom_level * 100).toFixed(0) + "%";
+  if (
+    ((zoom_level * 100).toFixed(0) >= 500 && type == 0.1) ||
+    ((zoom_level * 100).toFixed(0) <= 10 && type == -0.1)
+  )
+    return;
+  zoom_level += type;
+  let len = maps.length,
+    mult = 0;
+  if (current_header !== 2) {
+    len = maps.length / 2;
+    mult = 6;
+  }
+  for (let i = 0; i < len; i++) {
+    maps[i + mult].width = base_width * zoom_level;
+    maps[i + mult].height = base_height * zoom_level;
+  }
+  zoom_output[mult / 6].innerText = (zoom_level * 100).toFixed(0) + '%';
 }
 function location_choose(location) {
-    let scene;
-    if (location == 0) {
-        player.homekingdom = "Ignoma";
-        player.hometown = "Ebonfront";
-        scene = 0;
-    }
-    else if (location == 1) {
-        player.homekingdom = "Light Witesia";
-        player.hometown = "Basinfront";
-        scene = 100;
-    }
-    else if (location == 2) {
-        player.homekingdom = "The Luma Empire";
-        player.hometown = "Wildedenn";
-        scene = 200;
-    }
-    else if (location == 3) {
-        player.homekingdom = "The Kingdom of Dalia";
-        player.hometown = "Oxlight";
-        scene = 300;
-    }
-    else if (location == 4) {
-        player.homekingdom = "Zalrord";
-        player.hometown = "Bellecairn";
-        scene = 400;
-    }
-    else if (location == 5) {
-        player.homekingdom = "The Kingdom of Cataclite";
-        player.hometown = "Beargarde";
-        scene = 500;
-    }
-    else if (location == 6) {
-        player.homekingdom = "Seld";
-        player.hometown = "Westforest";
-        scene = 600;
-    }
-    generate_game(scene, true);
+  let scene;
+  if (location == 0) {
+    player.homekingdom = 'Ignoma';
+    player.hometown = 'Ebonfront';
+    scene = 0;
+  } else if (location == 1) {
+    player.homekingdom = 'Light Witesia';
+    player.hometown = 'Basinfront';
+    scene = 100;
+  } else if (location == 2) {
+    player.homekingdom = 'The Luma Empire';
+    player.hometown = 'Wildedenn';
+    scene = 200;
+  } else if (location == 3) {
+    player.homekingdom = 'The Kingdom of Dalia';
+    player.hometown = 'Oxlight';
+    scene = 300;
+  } else if (location == 4) {
+    player.homekingdom = 'Zalrord';
+    player.hometown = 'Bellecairn';
+    scene = 400;
+  } else if (location == 5) {
+    player.homekingdom = 'The Kingdom of Cataclite';
+    player.hometown = 'Beargarde';
+    scene = 500;
+  } else if (location == 6) {
+    player.homekingdom = 'Seld';
+    player.hometown = 'Westforest';
+    scene = 600;
+  }
+  generate_game(scene, true);
 }
 function submit_name() {
-    player.name = input_name.value;
-    let form = document.getElementById("name_submit");
-    form.parentNode.removeChild(form);
-    generate_map_menu();
+  player.name = input_name.value;
+  let form = document.getElementById('name_submit');
+  form.parentNode.removeChild(form);
+  generate_map_menu();
 }
